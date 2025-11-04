e1dc0626e338b49f25a8d130ecdb47cee10375dc
Sam <105767464+ScomosLastName@users.noreply.github.com>
2025-11-04 15:14:55 +1100

    Remove automatic fireworks timer logic
    Eliminated the server-side thread that automatically started and stopped fireworks based on the date and time. Added a new event to handle client requests for the updated configuration, and improved logging to include the source of config changes.

--- lhdc_fogoArtificio/server.lua
+++ lhdc_fogoArtificio/server.lua
@@ -1,28 +1,3 @@
-fireworking = false
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000)
-        local year, month, day, hours, minutes, seconds = os.date("%Y"), os.date("%m"), os.date("%d"), os.date("%H"), os.date("%M"), os.date("%S")
-        if((month == '01') and (day == '01') and (hours == '00') and (minutes == '00') and (seconds == '00'))then
-            print('Started Fireworks')
-            fireworking = true
-            TriggerClientEvent("lhdc_fogoArtificio:start", -1)
-        end
-        if((month == '01') and (day == '01') and (hours == '00') and (minutes == Config.FireworksTime) and (seconds == '00'))then
-            print('Stoped Fireworks')
-            fireworking = false
-            TriggerClientEvent("lhdc_fogoArtificio:stop", -1)
-        end
-    end
-end)
-
-RegisterServerEvent('lhdc_fogoArtificio:isFireworking')
-AddEventHandler('lhdc_fogoArtificio:isFireworking', function()
-    local src = source
-    TriggerClientEvent('lhdc_fogoArtificio:returnFireworkingStatus', src, fireworking)
-end)
-
 RegisterServerEvent('lhdc_fogoArtificio:updateConfig')
 AddEventHandler('lhdc_fogoArtificio:updateConfig', function(source, status, alias)
         -- Loop through Config entries and match alias
@@ -30,10 +5,15 @@ AddEventHandler('lhdc_fogoArtificio:updateConfig', function(source, status, alia
             if type(data) == "table" and data.Alias and data.Alias == alias then
                 data.enabled = status -- update enabled state
                 TriggerClientEvent('lhdc_fogoArtificio:updateEnabledStatus', -1, status, alias)
-                print(("Fireworks at '%s' set to: %s"):format(alias, tostring(status)))
+                print(("Fireworks at '%s' set to: %s, By: %s"):format(alias, tostring(status), source))
                 return
             end
         end
 
         print("No config entry found with alias:", alias)
+end)
+
+RegisterServerEvent('lhdc_fogoArtificio:requestUpdatedConfig')
+AddEventHandler('lhdc_fogoArtificio:requestUpdatedConfig', function(source)
+    TriggerClientEvent("lhdc_fogoArtificio:recieveUpdatedConfig", source, Config)
 end)

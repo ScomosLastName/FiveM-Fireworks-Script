14b88b61eae0888747681d57f7e2f5ab9cfdf929
Sam <105767464+ScomosLastName@users.noreply.github.com>
2025-11-06 12:33:41 +1100

    Finish Client Exports
    Finish all relevant exports and tidy Config file

--- lhdc_fogoArtificio/client_exports.lua
+++ lhdc_fogoArtificio/client_exports.lua
@@ -1,7 +1,19 @@
-exports('StartFireworks', function(name, status)
-    TriggerServerEvent('lhdc_fogoArtificio:updateConfig', -1, status, name)
+exports('ToggleFireworks', function(Alias, status)
+    TriggerServerEvent('lhdc_fogoArtificio:updateConfig', -1, status, Alias)
 end)
 
-exports('GetLocationsKeys', function()
+exports('GetLocationKeys', function()
     return Config.Keys
+end)
+
+exports('GetLocationData', function()
+    local result = {}
+
+    for _, key in ipairs(Config.Keys) do
+        if Config[key] and Config[key] then
+            table.insert(result, Config[key])
+        end
+    end
+
+    return result
 end)
